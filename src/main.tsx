df46fb578ec33bf28fbe08f46790f9224beee20e
Ansar Serikov <purplestick553@gmail.com>
2026-06-08 13:34:01 +0500

    feat(theme): remove theme toggles, the portal is always static white

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,18 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { queryClient } from "./lib/queryClient.ts"
 import { AuthProvider } from "./context/AuthProvider.tsx"
+import { Toaster } from "./components/ui/sonner.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <App />
+        <Toaster position="bottom-right" theme="light" />
       </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
